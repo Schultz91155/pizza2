4842afea06ea26a841e31e4ad9c2e81d9920d011
Schultz91155 <37634890+Schultz91155@users.noreply.github.com>
2023-03-24 21:50:32 +0300

    0.0.5

--- pizza2/PizzaViewController.swift
+++ pizza2/PizzaViewController.swift
@@ -58,7 +58,7 @@ extension PizzaViewController : UICollectionViewDataSource{
         let myCell = cell as? MyCollectionViewCell,
         let promotion = config?.productList.promotionList[indexPath.row]{
             myCell.titleLabel.text = promotion.title
-            let url = URL(string: promotion.imageLink)!
+            let url = URL(string: "https://github.com/Schultz91155/pizza2/blob/main/scr/Акции/1%20пицца%20и%20напиток.jpg?raw=true")!
             myCell.imagePizza.kf.setImage(with: Source.network(url))
         }
         return cell
